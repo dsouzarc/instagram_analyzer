09208509a158be76be530caeb8a6a75bfd793137
Ryan D'souza <dsouzarc@gmail.com>
2017-07-12 23:52:20 -0400

    Migration to utilize constants json file

--- InstagramAPI.py
+++ InstagramAPI.py
@@ -21,36 +21,45 @@ if sys.version_info.major == 3:
 from requests_toolbelt import MultipartEncoder
 
 class InstagramAPI:
+    
     API_URL = 'https://i.instagram.com/api/v1/'
-    DEVICE_SETTINTS = {
-    'manufacturer'      : 'Xiaomi',
-    'model'             : 'HM 1SW',
-    'android_version'   : 18,
-    'android_release'   : '4.3'
-    }
-    USER_AGENT = 'Instagram 9.2.0 Android ({android_version}/{android_release}; 320dpi; 720x1280; {manufacturer}; {model}; armani; qcom; en_US)'.format(**DEVICE_SETTINTS)
-    IG_SIG_KEY = '012a54f51c49aa8c5c322416ab1410909add32c966bbaa0fe3dc58ac43fd7ede'
-    EXPERIMENTS = 'ig_android_progressive_jpeg,ig_creation_growth_holdout,ig_android_report_and_hide,ig_android_new_browser,ig_android_enable_share_to_whatsapp,ig_android_direct_drawing_in_quick_cam_universe,ig_android_huawei_app_badging,ig_android_universe_video_production,ig_android_asus_app_badging,ig_android_direct_plus_button,ig_android_ads_heatmap_overlay_universe,ig_android_http_stack_experiment_2016,ig_android_infinite_scrolling,ig_fbns_blocked,ig_android_white_out_universe,ig_android_full_people_card_in_user_list,ig_android_post_auto_retry_v7_21,ig_fbns_push,ig_android_feed_pill,ig_android_profile_link_iab,ig_explore_v3_us_holdout,ig_android_histogram_reporter,ig_android_anrwatchdog,ig_android_search_client_matching,ig_android_high_res_upload_2,ig_android_new_browser_pre_kitkat,ig_android_2fac,ig_android_grid_video_icon,ig_android_white_camera_universe,ig_android_disable_chroma_subsampling,ig_android_share_spinner,ig_android_explore_people_feed_icon,ig_explore_v3_android_universe,ig_android_media_favorites,ig_android_nux_holdout,ig_android_search_null_state,ig_android_react_native_notification_setting,ig_android_ads_indicator_change_universe,ig_android_video_loading_behavior,ig_android_black_camera_tab,liger_instagram_android_univ,ig_explore_v3_internal,ig_android_direct_emoji_picker,ig_android_prefetch_explore_delay_time,ig_android_business_insights_qe,ig_android_direct_media_size,ig_android_enable_client_share,ig_android_promoted_posts,ig_android_app_badging_holdout,ig_android_ads_cta_universe,ig_android_mini_inbox_2,ig_android_feed_reshare_button_nux,ig_android_boomerang_feed_attribution,ig_android_fbinvite_qe,ig_fbns_shared,ig_android_direct_full_width_media,ig_android_hscroll_profile_chaining,ig_android_feed_unit_footer,ig_android_media_tighten_space,ig_android_private_follow_request,ig_android_inline_gallery_backoff_hours_universe,ig_android_direct_thread_ui_rewrite,ig_android_rendering_controls,ig_android_ads_full_width_cta_universe,ig_video_max_duration_qe_preuniverse,ig_android_prefetch_explore_expire_time,ig_timestamp_public_test,ig_android_profile,ig_android_dv2_consistent_http_realtime_response,ig_android_enable_share_to_messenger,ig_explore_v3,ig_ranking_following,ig_android_pending_request_search_bar,ig_android_feed_ufi_redesign,ig_android_video_pause_logging_fix,ig_android_default_folder_to_camera,ig_android_video_stitching_7_23,ig_android_profanity_filter,ig_android_business_profile_qe,ig_android_search,ig_android_boomerang_entry,ig_android_inline_gallery_universe,ig_android_ads_overlay_design_universe,ig_android_options_app_invite,ig_android_view_count_decouple_likes_universe,ig_android_periodic_analytics_upload_v2,ig_android_feed_unit_hscroll_auto_advance,ig_peek_profile_photo_universe,ig_android_ads_holdout_universe,ig_android_prefetch_explore,ig_android_direct_bubble_icon,ig_video_use_sve_universe,ig_android_inline_gallery_no_backoff_on_launch_universe,ig_android_image_cache_multi_queue,ig_android_camera_nux,ig_android_immersive_viewer,ig_android_dense_feed_unit_cards,ig_android_sqlite_dev,ig_android_exoplayer,ig_android_add_to_last_post,ig_android_direct_public_threads,ig_android_prefetch_venue_in_composer,ig_android_bigger_share_button,ig_android_dv2_realtime_private_share,ig_android_non_square_first,ig_android_video_interleaved_v2,ig_android_follow_search_bar,ig_android_last_edits,ig_android_video_download_logging,ig_android_ads_loop_count_universe,ig_android_swipeable_filters_blacklist,ig_android_boomerang_layout_white_out_universe,ig_android_ads_carousel_multi_row_universe,ig_android_mentions_invite_v2,ig_android_direct_mention_qe,ig_android_following_follower_social_context'
-    SIG_KEY_VERSION = '4'
 
-    #username            # Instagram username
-    #password            # Instagram password
-    #debug               # Debug
-    #uuid                # UUID
-    #device_id           # Device ID
-    #username_id         # Username ID
-    #token               # _csrftoken
-    #isLoggedIn          # Session status
-    #rank_token          # Rank token
-    #IGDataPath          # Data storage path
+    CONSTANTS_FILE_NAME = "Constants.json"
+    
+    USER_AGENT = None
+    IG_SIG_KEY = None
+    EXPERIMENTS = None
+    SIG_KEY_VERSION = None
 
-    def __init__(self, username, password, debug = False, IGDataPath = None):
+    username = None
+    password = None
+    username_id = None
+    is_logged_in = False
+    last_response = None
+    device_id = None
+    uuid = None
+    session = None
+    rank_token = None
+    token = None
+
+
+    def __init__(self, username, password, debug = False):
+
+        constants = json.load(open(self.CONSTANTS_FILE_NAME))
+
+        self.DEVICE_SETTINGS = constants["DEVICE_SETTINGS"]
+        self.USER_AGENT = constants["USER_AGENT"].format(**self.DEVICE_SETTINGS)
+        self.IG_SIG_KEY = constants["IG_SIG_KEY"]
+        self.EXPERIMENTS = constants["EXPERIMENTS"]
+        self.SIG_KEY_VERSION = constants["SIG_KEY_VERSION"]
+
         m = hashlib.md5()
         m.update(username.encode('utf-8') + password.encode('utf-8'))
         self.device_id = self.generateDeviceId(m.hexdigest())
         self.setUser(username, password)
-        self.isLoggedIn = False
-        self.LastResponse = None
+        self.is_logged_in = False
+        self.last_response = None
+
 
     def setUser(self, username, password):
         self.username = username
@@ -58,14 +67,14 @@ class InstagramAPI:
         self.uuid = self.generateUUID(True)
 
     def login(self, force = False):
-        if (not self.isLoggedIn or force):
-            self.s = requests.Session()
+        if (not self.is_logged_in or force):
+            self.session = requests.Session()
             # if you need proxy make something like this:
-            # self.s.proxies = {"https" : "http://proxyip:proxyport"}
+            # self.session.proxies = {"https" : "http://proxyip:proxyport"}
             if (self.SendRequest('si/fetch_headers/?challenge_type=signup&guid=' + self.generateUUID(False), None, True)):
 
                 data = {'phone_id'   : self.generateUUID(True),
-                        '_csrftoken' : self.LastResponse.cookies['csrftoken'],
+                        '_csrftoken' : self.last_response.cookies['csrftoken'],
                         'username'   : self.username,
                         'guid'       : self.uuid,
                         'device_id'  : self.device_id,
@@ -73,10 +82,10 @@ class InstagramAPI:
                         'login_attempt_count' : '0'}
 
                 if (self.SendRequest('accounts/login/', self.generateSignature(json.dumps(data)), True)):
-                    self.isLoggedIn = True
+                    self.is_logged_in = True
                     self.username_id = self.LastJson["logged_in_user"]["pk"]
                     self.rank_token = "%s_%s" % (self.username_id, self.uuid)
-                    self.token = self.LastResponse.cookies["csrftoken"]
+                    self.token = self.last_response.cookies["csrftoken"]
 
                     self.syncFeatures()
                     self.autoCompleteUserList()
@@ -129,7 +138,7 @@ class InstagramAPI:
         'photo'             : ('pending_media_%s.jpg'%upload_id, open(photo, 'rb'), 'application/octet-stream', {'Content-Transfer-Encoding':'binary'})
         }
         m = MultipartEncoder(data, boundary=self.uuid)
-        self.s.headers.update ({'X-IG-Capabilities' : '3Q4=',
+        self.session.headers.update ({'X-IG-Capabilities' : '3Q4=',
                                 'X-IG-Connection-Type' : 'WIFI',
                                 'Cookie2' : '$Version=1',
                                 'Accept-Language' : 'en-US',
@@ -137,7 +146,7 @@ class InstagramAPI:
                                 'Content-type': m.content_type,
                                 'Connection' : 'close',
                                 'User-Agent' : self.USER_AGENT})
-        response = self.s.post(self.API_URL + "upload/photo/", data=m.to_string())
+        response = self.session.post(self.API_URL + "upload/photo/", data=m.to_string())
         if response.status_code == 200:
             if self.configure(upload_id, photo, caption):
                 self.expose()
@@ -153,7 +162,7 @@ class InstagramAPI:
             '_uuid': self.uuid,
         }
         m = MultipartEncoder(data, boundary=self.uuid)
-        self.s.headers.update({'X-IG-Capabilities': '3Q4=',
+        self.session.headers.update({'X-IG-Capabilities': '3Q4=',
                                'X-IG-Connection-Type': 'WIFI',
                                'Host': 'i.instagram.com',
                                'Cookie2': '$Version=1',
@@ -162,7 +171,7 @@ class InstagramAPI:
                                'Content-type': m.content_type,
                                'Connection': 'keep-alive',
                                'User-Agent': self.USER_AGENT})
-        response = self.s.post(self.API_URL + "upload/video/", data=m.to_string())
+        response = self.session.post(self.API_URL + "upload/video/", data=m.to_string())
         if response.status_code == 200:
             body = json.loads(response.text)
             upload_url = body['video_upload_urls'][3]['url']
@@ -173,8 +182,8 @@ class InstagramAPI:
             request_size = int(math.floor(len(videoData) / 4))
             lastRequestExtra = (len(videoData) - (request_size * 3))
 
-            headers = copy.deepcopy(self.s.headers)
-            self.s.headers.update({'X-IG-Capabilities': '3Q4=',
+            headers = copy.deepcopy(self.session.headers)
+            self.session.headers.update({'X-IG-Capabilities': '3Q4=',
                                    'X-IG-Connection-Type': 'WIFI',
                                    'Cookie2': '$Version=1',
                                    'Accept-Language': 'en-US',
@@ -196,9 +205,9 @@ class InstagramAPI:
                 content_range = "bytes {start}-{end}/{lenVideo}".format(start=start, end=(end - 1),
                                                                         lenVideo=len(videoData)).encode('utf-8')
 
-                self.s.headers.update({'Content-Length': str(end - start), 'Content-Range': content_range, })
-                response = self.s.post(upload_url, data=videoData[start:start + length])
-            self.s.headers = headers
+                self.session.headers.update({'Content-Length': str(end - start), 'Content-Range': content_range, })
+                response = self.session.post(upload_url, data=videoData[start:start + length])
+            self.session.headers = headers
 
             if response.status_code == 200:
                 if self.configureVideo(upload_id, video, thumbnail, caption):
@@ -229,7 +238,7 @@ class InstagramAPI:
                 'source_width': clip.size[0],
                 'source_height': clip.size[1],
             },
-            'device': self.DEVICE_SETTINTS,
+            'device': self.DEVICE_SETTINGS,
             '_csrftoken': self.token,
             '_uuid': self.uuid,
             '_uid': self.username_id,
@@ -248,7 +257,7 @@ class InstagramAPI:
         '_uuid'         : self.uuid,
         'caption'       : caption,
         'upload_id'     : upload_id,
-        'device'        : self.DEVICE_SETTINTS,
+        'device'        : self.DEVICE_SETTINGS,
         'edits'         : {
             'crop_original_size': [w * 1.0, h * 1.0],
             'crop_center'       : [0.0, 0.0],
@@ -598,11 +607,11 @@ class InstagramAPI:
         return False
 
     def SendRequest(self, endpoint, post = None, login = False):
-        if (not self.isLoggedIn and not login):
+        if (not self.is_logged_in and not login):
             raise Exception("Not logged in!\n")
             return;
 
-        self.s.headers.update ({'Connection' : 'close',
+        self.session.headers.update ({'Connection' : 'close',
                                 'Accept' : '*/*',
                                 'Content-type' : 'application/x-www-form-urlencoded; charset=UTF-8',
                                 'Cookie2' : '$Version=1',
@@ -610,19 +619,19 @@ class InstagramAPI:
                                 'User-Agent' : self.USER_AGENT})
 
         if (post != None): # POST
-            response = self.s.post(self.API_URL + endpoint, data=post) # , verify=False
+            response = self.session.post(self.API_URL + endpoint, data=post) # , verify=False
         else: # GET
-            response = self.s.get(self.API_URL + endpoint) # , verify=False
+            response = self.session.get(self.API_URL + endpoint) # , verify=False
 
         if response.status_code == 200:
-            self.LastResponse = response
+            self.last_response = response
             self.LastJson = json.loads(response.text)
             return True
         else:
             print ("Request return " + str(response.status_code) + " error!")
             # for debugging
             try:
-                self.LastResponse = response
+                self.last_response = response
                 self.LastJson = json.loads(response.text)
             except:
                 pass
